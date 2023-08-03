e894f6606367e2111b4beaf6d4010e73bb171a2d
Kale Pallavi Arjun <121017623+KalePallavi@users.noreply.github.com>
2023-08-03 12:48:22 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+// App.js
+import React from 'react';
+import Login from './Component/Login.js';
+
+const App = () => {
+  return (
+    <div>
+       <Login />  
+    </div>
+  );
+};
+export default App;
