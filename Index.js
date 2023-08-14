a8464d38b65c52683d3dbd79108e98f743f53c8c
Kale Pallavi Arjun <121017623+KalePallavi@users.noreply.github.com>
2023-08-14 13:04:39 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import Demo10 from "./Demo10.jsx";
+ReactDOM.render(
+  <>
+   <Demo10></Demo10>
+  </>
+
+  ,document.getElementById('root'));
+
+
